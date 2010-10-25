1885f8d0a88437dcecfe2140fba987c1a7572172
Scott McLeod <halcyonblue@gmail.com>
2010-10-25 15:15:20 -0400

    Adding findvms.sh

---
+++ findvms.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+IFS='
+'
+
+IPS=`ifconfig -a | grep "inet addr" | ruby -e 'ARGF.each_line { |line| print line[/addr:([0-9]+\.[0-9]+\.[0-9]+)/, 1]+".0/24\n" }'`
+VMS=`VBoxManage list runningvms | grep "^\""`
+
+for SUBNET in $IPS
+do
+  nmap -sP $SUBNET  > /dev/null 2>&1
+done
+
+for VM in $VMS
+do
+  MACHINE=`echo $VM |ruby -e 'puts ARGF.readline[/"([A-z]+)"/, 1]'`
+  MAC=`VBoxManage showvminfo "$MACHINE"| grep MAC: |ruby -e 'puts ARGF.readline[/MAC: (([0-9]|[A-Z])+)/, 1].downcase.scan(/(..)/).join(":")'`
+  arp -vn | grep $MAC
+done
