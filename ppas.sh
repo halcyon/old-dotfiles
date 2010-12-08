a723c48c52f47b3011abcfe5917f00b765473fce
Scott McLeod <halcyonblue@gmail.com>
2010-12-08 11:12:09 -0500

    Adding vim-gtk and build-essential

--- ppas.sh
+++ ppas.sh
@@ -5,3 +5,8 @@ apt-add-repository ppa:sevenmachines/flash
 apt-add-repository ppa:git-core/ppa
 
 apt-get update
+
+apt-get install -y chromium-browser flashplugin64-installer
+apt-get install -y openssh-server vim-gtk build-essential
+
+apt-get -y upgrade
