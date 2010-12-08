4723c3f754a021e7e27303656b0ec2cc518cf9b1
Scott McLeod <halcyonblue@gmail.com>
2010-12-08 12:05:59 -0500

    Make package install non-interactive

--- packages.sh
+++ packages.sh
@@ -6,9 +6,10 @@ apt-add-repository ppa:git-core/ppa
 apt-add-repository ppa:pidgin-developers/ppa
 
 apt-get update
-apt-get purge empathy vim-tiny
+apt-get -y purge empathy vim-tiny
 apt-get -y upgrade
 
-apt-get install -y chromium-browser flashplugin64-installer
-apt-get install -y openssh-server vim-gtk build-essential
-apt-get install -y lxc libvirt-bin
+apt-get -y install chromium-browser flashplugin64-installer vim-gtk pidgin
+apt-get -y install openssh-server
+apt-get -y install build-essential libreadline-dev libssl-dev
+apt-get -y install lxc libvirt-bin
