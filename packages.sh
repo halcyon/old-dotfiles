6051c5009d87d3ffc92612db2925b8e2977dd455
Scott McLeod <halcyonblue@gmail.com>
2010-12-09 19:08:28 -0500

    Adding rv-wk-smcl-02.conf and git-svn

--- packages.sh
+++ packages.sh
@@ -9,7 +9,8 @@ apt-get update
 apt-get -y purge empathy vim-tiny
 apt-get -y upgrade
 
+apt-get -y install git git-svn
 apt-get -y install chromium-browser flashplugin64-installer vim-gtk pidgin
 apt-get -y install openssh-server
-apt-get -y install build-essential libreadline-dev libssl-dev
+apt-get -y install build-essential libreadline-dev libssl-dev dput
 apt-get -y install lxc libvirt-bin
