926a7c12693fa12ccbfd71f33a52576e0ac6dbec
Scott McLeod <halcyonblue@gmail.com>
2010-12-16 15:42:39 -0500

    Adding Joshua Born reccomended vim plugins

--- .vimrc
+++ .vimrc
@@ -14,10 +14,12 @@ set shiftwidth=2
 set tabstop=2
 set expandtab
 
-autocmd FileType ruby,eruby set omnifunc=rubycomplete#Complete
-autocmd FileType ruby,eruby let g:rubycomplete_buffer_loading = 1
-autocmd FileType ruby,eruby let g:rubycomplete_rails = 1
-autocmd FileType ruby,eruby let g:rubycomplete_classes_in_global = 1
+"autocmd FileType ruby,eruby set omnifunc=rubycomplete#Complete
+"autocmd FileType ruby,eruby let g:rubycomplete_buffer_loading = 1
+"autocmd FileType ruby,eruby let g:rubycomplete_rails = 1
+"autocmd FileType ruby,eruby let g:rubycomplete_classes_in_global = 1
+let mapleader = "\\"
+map <Leader>n :NERDTreeToggle<CR>
 
 autocmd FileType make setlocal noexpandtab
 autocmd BufEnter *.patch setlocal noexpandtab
--- packages.sh
+++ packages.sh
@@ -13,6 +13,7 @@ apt-add-repository ppa:sevenmachines/flash
 apt-add-repository ppa:git-core/ppa
 apt-add-repository ppa:pidgin-developers/ppa
 apt-add-repository ppa:halcyonblue/synergy
+apt-add-repository ppa:halcyonblue/ffmpeg
 apt-add-list virtualbox 'deb http://download.virtualbox.org/virtualbox/debian maverick non-free'
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 
@@ -22,7 +23,8 @@ apt-get -y purge empathy vim-tiny
 apt-get -y upgrade
 
 apt-get -y install git git-svn
-apt-get -y install chromium-browser flashplugin64-installer vim-gtk pidgin
+apt-get -y install chromium-browser flashplugin64-installer vim-gtk ctags ack-grep pidgin
+ln -s /usr/bin/ack-grep /usr/bin/ack
 apt-get -y install openssh-server
 apt-get -y install build-essential libreadline-dev libssl-dev dput
 apt-get -y install lxc libvirt-bin virtualbox-3.2
