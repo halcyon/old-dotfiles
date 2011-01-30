bde4f4c41fe7085beabfc5db90a08ddeaef82af2
Scott McLeod <halcyonblue@gmail.com>
2011-01-30 12:33:48 -0500

    bundle up ack

--- packages.sh
+++ packages.sh
@@ -39,7 +39,9 @@ aptitude -y install git git-svn
 aptitude -y install chromium-browser flashplugin64-installer vim-gtk ctags ack-grep pidgin calibre
 ln -s /usr/bin/ack-grep /usr/bin/ack
 aptitude -y install openssh-server
-aptitude -y install build-essential libreadline-dev libssl-dev curl mysql-server libmysqlclient-dev dput
+aptitude -y install mysql-server libmysqlclient-dev dput
+
+aptitude -y install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev
 
 add-cgroup
 aptitude -y install lxc libvirt-bin
