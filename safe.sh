4e146931741d1fdb0edf07a27fbcd474b173b98c
Scott McLeod <scott.mcleod@theice.com>
2011-04-06 14:29:10 -0400

    Use port 3333

--- safe.sh
+++ safe.sh
@@ -2,7 +2,7 @@
 if [ ! -f `dirname $0`/safe.lock ]
 then
   touch `dirname $0`/safe.lock
-  iproxy 2222 22 &
+  iproxy 3333 22 &
   ssh -N -D localhost:8888 mob
   pkill -term iproxy
   rm -f `dirname $0`/safe.lock
