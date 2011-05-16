2f38c74d71b2acc59bb2b7cbb57d68e6d228c9ae
Scott McLeod <scott.mcleod@theice.com>
2011-05-16 12:07:25 -0400

    Make more generic

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,10 +1,9 @@
 log_level                :info
 log_location             STDOUT
-node_name                'smcleod'
-client_key               '/home/smcleod/.chef/smcleod.pem'
+client_key               '~/.chef/client.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/smcleod/.chef/validation.pem'
+validation_key           '~/.chef/validation.pem'
 chef_server_url          'http://irulan.homeunix.net:4000'
 cache_type               'BasicFile'
-cache_options( :path => '/home/smcleod/.chef/checksums' )
+cache_options( :path => '~/.chef/checksums' )
 cookbook_path            [ "~/chef-repo/cookbooks", "~/chef-repo/site-cookbooks" ]
