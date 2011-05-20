8826e151b3bd0bad9fa476a62478b4d0cf9be9d6
Scott McLeod <scott.mcleod@theice.com>
2011-05-20 18:54:30 -0400

    Switch to zeddworks.com

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,7 +4,7 @@ log_location             STDOUT
 client_key               "#{ENV['HOME']}/.chef/client.pem"
 validation_client_name   'chef-validator'
 validation_key           "#{ENV['HOME']}/.chef/validation.pem"
-chef_server_url          'http://10.3.102.31:4000'
+chef_server_url          'http://zeddworks.com:4000'
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            [ "#{ENV['HOME']}/chef-repo/cookbooks", "#{ENV['HOME']}/chef-repo/site-cookbooks" ]
