dda4f7c9fea82017782bc5f752d0d033ca1eefe8
Scott McLeod <scott.mcleod@theice.com>
2011-05-26 13:20:41 -0400

    Use arrays for PPAs, and conditionalize adding them

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. /etc/lsb-release 
+
 apt-add-list() {
   sourcesDir='/etc/apt/sources.list.d'
   if [ ! -f $sourcesDir/$1.list ]
@@ -8,17 +10,20 @@ apt-add-list() {
   fi
 }
 
+PPAS=(git-core/ppa sevenmachines/flash)
+
 RVM_DEBS=(build-essential bison openssl libreadline6 libreadline6-dev curl
 git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev
 sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev)
 
 WEB_DEBS=(firefox chromium-browser flashplugin64-installer)
 
-VIM_DEBS=(vim-gtk ctags ack-grep)
+VIM_DEBS=(vim-gtk exuberant-ctags ack-grep)
 
 SYSTEM_DEBS=(ppa-purge git-svn openssh-server synergy virtualbox-4.0)
 
 MISC_DEBS=(pidgin calibre pdfedit gtk-recordMyDesktop)
+
 
 dpkg-query -s aptitude > /dev/null 2>&1
 if [ "$?" -eq 1 ]
@@ -27,18 +32,24 @@ then
 fi
 
 dpkg-query -s empathy vim-tiny > /dev/null 2>&1
-if [ "$?" -eq 1 ]
+if [ "$?" -eq 0 ]
 then
   aptitude -y purge empathy vim-tiny
 fi
 
-apt-add-repository ppa:git-core/ppa
-apt-add-repository ppa:sevenmachines/flash
+for ppa in ${PPAS[*]}
+do
+  list=`echo $ppa | tr '/' '-'`-$DISTRIB_CODENAME
+  if [ ! -f "/etc/apt/sources.list.d/$list.list" ]
+  then
+    apt-add-repository ppa:$ppa
+  fi
+done
 apt-add-list virtualbox 'deb http://download.virtualbox.org/virtualbox/debian natty contrib'
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - > /dev/null 2>&1
 
-aptitude update
-aptitude -y upgrade
+#aptitude update
+#aptitude -y upgrade
 
 dpkg-query -s ${WEB_DEBS[*]} > /dev/null 2>&1
 if [ "$?" -eq 1 ]
@@ -50,7 +61,6 @@ dpkg-query -s ${VIM_DEBS[*]} > /dev/null 2>&1
 if [ "$?" -eq 1 ]
 then
   aptitude -y install ${VIM_DEBS[*]}
-  ln -sf /usr/bin/ack-grep /usr/bin/ack
 fi
 
 dpkg-query -s ${SYSTEM_DEBS[*]} > /dev/null 2>&1
@@ -71,8 +81,9 @@ then
   aptitude -y install ${RVM_DEBS[*]}
 fi
 
+ln -sf /usr/bin/ack-grep /usr/bin/ack
 
-aptitude autoclean
+#aptitude autoclean
 
 if [ ! -d "/usr/local/rvm" ]
 then
