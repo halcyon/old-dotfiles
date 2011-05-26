f8af9d9b3de4d9081b68985728864dbefb402ae9
Scott McLeod <scott.mcleod@theice.com>
2011-05-26 14:34:35 -0400

    Completely overhaul bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -10,6 +10,30 @@ apt-add-list() {
   fi
 }
 
+apt-install() {
+  dpkg-query -s $1 > /dev/null 2>&1
+  if [ "$?" -eq 1 ]
+  then
+    apt-get -y install $1
+  fi
+}
+
+apt-purge() {
+  dpkg-query -s $1 > /dev/null 2>&1
+  if [ "$?" -eq 0 ]
+  then
+    aptitude -y purge $1
+  fi
+}
+
+aptitude-install() {
+  dpkg-query -s $1 > /dev/null 2>&1
+  if [ "$?" -eq 1 ]
+  then
+    aptitude -y install $1
+  fi
+}
+
 gem-install() {
   gem list | grep $1 > /dev/null 2>&1
   if [ "$?" -eq 1 ]
@@ -18,97 +42,66 @@ gem-install() {
   fi
 }
 
+PURGE_DEBS=(empathy vim-tiny)
 PPAS=(git-core/ppa sevenmachines/flash)
 
+SYSTEM_DEBS=(ppa-purge git-svn openssh-server synergy virtualbox-4.0)
 RVM_DEBS=(build-essential bison openssl libreadline6 libreadline6-dev curl
 git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev
 sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev)
-
-WEB_DEBS=(firefox chromium-browser flashplugin64-installer)
-
 VIM_DEBS=(vim-gtk exuberant-ctags ack-grep)
-
-SYSTEM_DEBS=(ppa-purge git-svn openssh-server synergy virtualbox-4.0)
-
+WEB_DEBS=(firefox chromium-browser flashplugin64-installer)
 MISC_DEBS=(pidgin calibre pdfedit gtk-recordMyDesktop)
 
 GEMS=(vagrant veewee chef)
 
-dpkg-query -s aptitude > /dev/null 2>&1
-if [ "$?" -eq 1 ]
-then
-  apt-get -y install aptitude
-fi
+setup-debian-packages() {
+  for ppa in ${PPAS[*]}
+  do
+    list=`echo $ppa | tr '/' '-'`-$DISTRIB_CODENAME
+    if [ ! -f "/etc/apt/sources.list.d/$list.list" ]
+    then
+      apt-add-repository ppa:$ppa
+    fi
+  done
+  apt-add-list virtualbox 'deb http://download.virtualbox.org/virtualbox/debian natty contrib'
+  wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - > /dev/null 2>&1
 
-dpkg-query -s empathy vim-tiny > /dev/null 2>&1
-if [ "$?" -eq 0 ]
-then
-  aptitude -y purge empathy vim-tiny
-fi
+  apt-install aptitude
+  apt-purge ${PURGE_DEBS[*]}
+  aptitude update
+  aptitude -y upgrade
 
-for ppa in ${PPAS[*]}
-do
-  list=`echo $ppa | tr '/' '-'`-$DISTRIB_CODENAME
-  if [ ! -f "/etc/apt/sources.list.d/$list.list" ]
+  INSTALL_ARRAYS=`declare -a | grep DEBS | grep -v PURGE_DEBS | awk -F'=' '{print $1}' | awk -F'-a ' '{print $2}'`
+  for array in ${INSTALL_ARRAYS[*]}
+  do
+    eval aptitude-install \"\${$array[*]}\"
+  done
+  ln -sf /usr/bin/ack-grep /usr/bin/ack
+  aptitude autoclean
+}
+
+
+setup-ruby() {
+  if [ ! -d "/usr/local/rvm" ]
   then
-    apt-add-repository ppa:$ppa
+    bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+    . /etc/profile
   fi
-done
-apt-add-list virtualbox 'deb http://download.virtualbox.org/virtualbox/debian natty contrib'
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - > /dev/null 2>&1
 
-#aptitude update
-#aptitude -y upgrade
+  rvm list | grep ree > /dev/null 2>&1
+  if [ "$?" -eq 1 ]
+  then
+    echo "gem: --no-rdoc --no-ri" > /etc/gemrc
+    rvm install ree
+    rvm --create --default ree@system
+  fi
 
-dpkg-query -s ${WEB_DEBS[*]} > /dev/null 2>&1
-if [ "$?" -eq 1 ]
-then
-  aptitude -y install ${WEB_DEBS[*]}
-fi
+  for gem in ${GEMS[*]}
+  do
+    gem-install $gem
+  done
+}
 
-dpkg-query -s ${VIM_DEBS[*]} > /dev/null 2>&1
-if [ "$?" -eq 1 ]
-then
-  aptitude -y install ${VIM_DEBS[*]}
-fi
-
-dpkg-query -s ${SYSTEM_DEBS[*]} > /dev/null 2>&1
-if [ "$?" -eq 1 ]
-then
-  aptitude -y install ${SYSTEM_DEBS[*]}
-fi
-
-dpkg-query -s ${MISC_DEBS[*]} > /dev/null 2>&1
-if [ "$?" -eq 1 ]
-then
-  aptitude -y install ${SYSTEM_DEBS[*]}
-fi
-
-dpkg-query -s ${RVM_DEBS[*]} > /dev/null 2>&1
-if [ "$?" -eq 1 ]
-then
-  aptitude -y install ${RVM_DEBS[*]}
-fi
-
-ln -sf /usr/bin/ack-grep /usr/bin/ack
-
-#aptitude autoclean
-
-if [ ! -d "/usr/local/rvm" ]
-then
-  bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
-  . /etc/profile
-fi
-
-rvm list | grep ree > /dev/null 2>&1
-if [ "$?" -eq 1 ]
-then
-  echo "gem: --no-rdoc --no-ri" > /etc/gemrc
-  rvm install ree
-  rvm --create --default ree@system
-fi
-
-for gem in ${GEMS[*]}
-do
-  gem-install $gem
-done
+setup-debian-packages
+setup-ruby
