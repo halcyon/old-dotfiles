451e6d99a22b1712b72208b8e107795dd842c3f7
Scott McLeod <scott.mcleod@theice.com>
2011-05-26 10:45:15 -0400

    Doing cleanup of ubuntu bootstrapping

--- bin/packages.sh
+++ bin/packages.sh
@@ -8,31 +8,12 @@ apt-add-list() {
   fi
 }
 
-add-cgroup() {
-  grep cgroup /etc/fstab > /dev/null
-  if [ $? -ne 0 ]
-  then
-    echo -e "none\t/cgroup\tcgroup\tdefaults\t0\t0" >> /etc/fstab
-  fi
-  mkdir -p /cgroup
-  mount /cgroup
-}
-
-#apt-get -y install aptitude
-#aptitude -y install python-software-properties ppa-purge
-
 aptitude -y purge empathy vim-tiny
 aptitude install ppa-purge
 
-apt-add-repository ppa:chromium-daily/stable
-apt-add-repository ppa:sevenmachines/flash
 apt-add-repository ppa:git-core/ppa
-apt-add-repository ppa:mozillateam/firefox-stable
-apt-add-repository ppa:pidgin-developers/ppa
-apt-add-repository ppa:halcyonblue/synergy
-apt-add-repository ppa:halcyonblue/ffmpeg
-apt-add-repository ppa:halcyonblue/calibre
-apt-add-list virtualbox 'deb http://download.virtualbox.org/virtualbox/debian maverick contrib'
+apt-add-repository ppa:sevenmachines/flash
+apt-add-list virtualbox 'deb http://download.virtualbox.org/virtualbox/debian natty contrib'
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 
 aptitude update
@@ -42,15 +23,31 @@ aptitude -y install git git-svn
 aptitude -y install chromium-browser flashplugin64-installer vim-gtk ctags ack-grep pidgin calibre pdfedit
 ln -s /usr/bin/ack-grep /usr/bin/ack
 aptitude -y install openssh-server
-aptitude -y install mysql-server libmysqlclient-dev
 
-#aptitude -y install dput
 
 aptitude -y install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
-
-# add-cgroup
-# aptitude -y install lxc libvirt-bin
 
 aptitude -y install synergy
 aptitude -y install virtualbox-4.0
 aptitude -y install gtk-recordMyDesktop
+
+#We no longer use LXC
+#add-cgroup() {
+#  grep cgroup /etc/fstab > /dev/null
+#  if [ $? -ne 0 ]
+#  then
+#    echo -e "none\t/cgroup\tcgroup\tdefaults\t0\t0" >> /etc/fstab
+#  fi
+#  mkdir -p /cgroup
+#  mount /cgroup
+#}
+
+# add-cgroup
+# aptitude -y install lxc libvirt-bin
+
+#Needed for publishing to a PPA
+#aptitude -y install dput
+
+#No longer needed in Natty
+#apt-get -y install aptitude
+#aptitude -y install python-software-properties ppa-purge
