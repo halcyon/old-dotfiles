e9892a9860d641ca8bd971fb1012858852b689d2
Scott McLeod <halcyonblue@gmail.com>
2011-06-06 16:11:22 -0400

    Remove nukecookbooks and nukedatabags

--- bin/nukecookbooks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `knife cookbook list | awk '{print $1}'` ; do knife cookbook delete -y $i; done
--- bin/nukedatabags.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `knife data bag list` ; do knife data bag delete -y $i; done
