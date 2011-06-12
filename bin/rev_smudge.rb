c29bf9e9890850e691961563a9f7240c8b1eeda0
Scott McLeod <halcyonblue@gmail.com>
2011-06-11 23:16:19 -0400

    Use 6 character hex versions

--- bin/rev_smudge.rb
+++ bin/rev_smudge.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 data = STDIN.read
-rev = `git rev-parse master | cut -b1-7`.chomp
+rev = `git rev-parse master | cut -b1-6`.chomp
 puts data.gsub('$Rev$', '$Rev: ' + rev.to_s + ' $')
